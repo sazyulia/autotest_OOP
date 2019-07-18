15a926921accfb0a9de5dbfbbf5197d6ecb799c5
unknown <sazyulia@yandex.ru>
2019-07-18 11:53:09 +0300

    исправления языка по умолчанию

--- conftest.py
+++ conftest.py
@@ -1,15 +1,10 @@
 import pytest
 from selenium import webdriver
 
-from selenium.webdriver.chrome.options import Options
-
-options = Options()
-options.add_experimental_option('prefs', {'intl.accept_languages': 'en'})
-browser = webdriver.Chrome(options=options)
-
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="en")
 
 @pytest.fixture(scope="function")
 def browser(request):
