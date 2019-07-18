38e2bb11f70a2cbf4f37ff6e04de834fb06dfdd6
unknown <sazyulia@yandex.ru>
2019-07-18 14:12:45 +0300

    Добавление исключения, перенос селекторов в locators

--- base_page.py
+++ base_page.py
@@ -1,8 +1,16 @@
 #создаем класс
 class BasePage(object):
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
 
 # открываем страницу в браузере
     def open(self):
